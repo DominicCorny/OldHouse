c4b5c93c40571737f57122fcc9d86c9ff021a627
DomiCore <kern.dominic@web.de>
2017-06-15 14:06:11 +0200

    Initial commit

---
+++ main.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Page Title</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<ul>
+    <li><a class="active href="default.asp">OldHouse</a></li>
+    <li><a href="news.asp">Bilder</a></li>
+    <li><a href="contact.asp">Stammtisch</a></li>
+    <li><a href="about.asp">Gästebuch</a></li>
+    <li style="float:right"><a href="about.asp">About</a></li>
+</ul>
+
+<h1>This is a Heading</h1>
+<p>This is a paragraph.</p>
+
+</body>
+</html>
